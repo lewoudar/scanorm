49c3fafb7284634ac9fd90a82a2a9a446f3e0d36
le_woudar <rollandkev@yahoo.fr>
2022-12-06 12:15:24 +0100

    feat(apps): implemented ready method
    Now the project will scan models when a Django app starts

--- scanorm/apps.py
+++ scanorm/apps.py
@@ -1,8 +1,10 @@
 from django.apps import AppConfig
 
+from .inspector import create_model_schema
+
 
 class ScanormConfig(AppConfig):
     name = 'scanorm'
 
     def ready(self):
-        pass
+        create_model_schema()
